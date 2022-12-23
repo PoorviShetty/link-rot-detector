db74eb850f4a7a26213977aab72c45c44e56d5c0
Poorvi <poorvistudies@gmail.com>
2022-12-23 21:01:58 +0530

    recursively check links

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ import requests
 driver = webdriver.Firefox(executable_path='geckodriver.exe')
 
 # change link 
-driver.get("https://www.adobe.com/404")
+links = ["https://www.drlinkcheck.com/"]
 
-elems = driver.find_elements(By.XPATH, "//a[@href]")
-for elem in elems:
-    if (requests.get(elem.get_attribute("href")).status_code) != 200:
-        print("Error: " + elem.get_attribute("href"))
+count = 0
+
+
+for link in links:
+    driver.get(link)
+    count += 1
+    elems = driver.find_elements(By.XPATH, "//a[@href]")
+    for elem in elems:
+        if "mailto" not in elem.get_attribute("href"):
+            links.append(elem.get_attribute("href"))
+            if (requests.get(elem.get_attribute("href")).status_code) != 200:
+                print("Error: " + elem.get_attribute("href"))
+
+    links.remove(link)
+
+    if count > 5:
+        print("Max depth exceeded")
+        break
+    #print(links)
